70c463bdc26c6f32c5f7d0f3d32767df3396b5db
JeonghoWin <zhfighting2014@163.com>
2016-06-15 16:51:53 +0800

    creat TabLayout

---
+++ Metarial/app/src/main/java/com/jeongho/metarial/adapter/ContentPagerAdapter.java
@@ -0,0 +1,47 @@
+package com.jeongho.metarial.adapter;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by Jeongho on 2016/6/15.
+ */
+public class ContentPagerAdapter extends PagerAdapter{
+
+    private List<View> mViewList;
+    private List<String> mTitles;
+
+    public ContentPagerAdapter(List<View> viewList, List<String> titles) {
+        mViewList = viewList;
+        mTitles = titles;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        container.addView(mViewList.get(position), 0);
+        return mViewList.get(position);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView(mViewList.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return mViewList.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mTitles.get(position);
+    }
+}
