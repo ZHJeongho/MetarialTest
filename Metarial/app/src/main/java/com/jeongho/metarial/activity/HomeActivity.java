2dbcfc7b58cd00bcece83eaee53bf9ec354c79ab
JeonghoWin <zhfighting2014@163.com>
2016-06-16 16:43:12 +0800

    add some component
    Toolbar
    DrawerLayout

--- Metarial/app/src/main/java/com/jeongho/metarial/activity/HomeActivity.java
+++ Metarial/app/src/main/java/com/jeongho/metarial/activity/HomeActivity.java
@@ -1,9 +1,7 @@
 package com.jeongho.metarial.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
@@ -11,6 +9,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,85 +18,94 @@ import android.view.View;
 import com.jeongho.metarial.R;
 import com.jeongho.metarial.adapter.ContentPagerAdapter;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import java.util.LinkedList;
+import java.util.List;
 
+/**
+ * Created by Jeongho on 2016/6/16.
+ */
+public class HomeActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener, NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar mToolbar;
     private TabLayout mTab;
     private ViewPager mContentVp;
     private ContentPagerAdapter mPagerAdapter;
 
     private int[] mLayoutIds = new int[]{R.layout.content_pager_1, R.layout.content_pager_2,
-    R.layout.content_pager_3};
-
+            R.layout.content_pager_3};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
         initView();
-
     }
 
     private void initView() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle("骑行邦");
+        mToolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
+        setSupportActionBar(mToolbar);
+        mToolbar.setNavigationIcon(R.drawable.ic_menu_white_24dp);
+        mToolbar.setOnMenuItemClickListener(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mContentVp = (ViewPager) findViewById(R.id.vp_content);
+        List<View> views = new LinkedList<>();
+        LayoutInflater l = LayoutInflater.from(this);
+
+        for (int layoutId : mLayoutIds){
+            View v = l.inflate(layoutId, null);
+            views.add(v);
+        }
+
+        List<String> titles = new LinkedList<>();
+        titles.add("首页");
+        titles.add("骑圈");
+        titles.add("发现");
+        mPagerAdapter = new ContentPagerAdapter(views, titles);
+        mContentVp.setAdapter(mPagerAdapter);
+
+        mTab = (TabLayout) findViewById(R.id.tab);
+        mTab.setupWithViewPager(mContentVp);
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-
-
-
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
+
+//    private Toolbar.OnMenuItemClickListener OnMenuItemClick = new Toolbar.OnMenuItemClickListener() {
+//        @Override
+//        public boolean onMenuItemClick(MenuItem item) {
+//            return false;
+//        }
+//    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.menu_home, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_notification:
+                Log.d("Menu", "notification");
+                break;
+            case R.id.action_search:
+                Log.d("Menu", "search");
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
+        return false;
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
