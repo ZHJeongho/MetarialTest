0a9e8054dbd0ae5380c632232aba0bea8c7f9023
JeonghoMac <zhfighting2014@163.com>
2016-06-16 22:41:14 +0800

    change fragment

--- Metarial/app/src/main/java/com/jeongho/metarial/activity/HomeActivity.java
+++ Metarial/app/src/main/java/com/jeongho/metarial/activity/HomeActivity.java
@@ -3,6 +3,9 @@ package com.jeongho.metarial.activity;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
@@ -10,13 +13,19 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.jeongho.metarial.R;
+import com.jeongho.metarial.adapter.ContentFragmentAdapter;
 import com.jeongho.metarial.adapter.ContentPagerAdapter;
+import com.jeongho.metarial.fragment.DiscoveryFragment;
+import com.jeongho.metarial.fragment.HomeFragment;
+import com.jeongho.metarial.fragment.MyAttentionFragment;
+import com.jeongho.metarial.fragment.MyCollectFragment;
+import com.jeongho.metarial.fragment.MyPostsFragment;
+import com.jeongho.metarial.fragment.RideCycleFragment;
+import com.jeongho.metarial.fragment.SettingFragment;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -30,6 +39,12 @@ public class HomeActivity extends AppCompatActivity implements Toolbar.OnMenuIte
     private TabLayout mTab;
     private ViewPager mContentVp;
     private ContentPagerAdapter mPagerAdapter;
+    private ContentFragmentAdapter mContentFragmentAdapter;
+
+    private MyCollectFragment mCollectFragment;
+    private MyAttentionFragment mAttentionFragment;
+    private MyPostsFragment mPostsFragment;
+    private SettingFragment mSettingFragment;
 
     private int[] mLayoutIds = new int[]{R.layout.content_pager_1, R.layout.content_pager_2,
             R.layout.content_pager_3};
@@ -42,29 +57,34 @@ public class HomeActivity extends AppCompatActivity implements Toolbar.OnMenuIte
     }
 
     private void initView() {
+        //初始化Toolbar
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mToolbar.setTitle("骑行邦");
         mToolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
         setSupportActionBar(mToolbar);
         mToolbar.setNavigationIcon(R.drawable.ic_menu_white_24dp);
         mToolbar.setOnMenuItemClickListener(this);
-
+        //初始化ViewPager
         mContentVp = (ViewPager) findViewById(R.id.vp_content);
-        List<View> views = new LinkedList<>();
-        LayoutInflater l = LayoutInflater.from(this);
-
-        for (int layoutId : mLayoutIds){
-            View v = l.inflate(layoutId, null);
-            views.add(v);
-        }
 
         List<String> titles = new LinkedList<>();
         titles.add("首页");
         titles.add("骑圈");
         titles.add("发现");
-        mPagerAdapter = new ContentPagerAdapter(views, titles);
-        mContentVp.setAdapter(mPagerAdapter);
 
+        List<Fragment> fragmentList = new LinkedList<>();
+        HomeFragment homeFragment = new HomeFragment();
+        RideCycleFragment rideCycleFragment = new RideCycleFragment();
+        DiscoveryFragment discoveryFragment = new DiscoveryFragment();
+
+        fragmentList.add(homeFragment);
+        fragmentList.add(rideCycleFragment);
+        fragmentList.add(discoveryFragment);
+
+        mContentFragmentAdapter = new ContentFragmentAdapter(getSupportFragmentManager(), fragmentList, titles);
+        mContentVp.setAdapter(mContentFragmentAdapter);
+
+        //TabLayout与ViewPager绑定
         mTab = (TabLayout) findViewById(R.id.tab);
         mTab.setupWithViewPager(mContentVp);
 
@@ -111,19 +131,36 @@ public class HomeActivity extends AppCompatActivity implements Toolbar.OnMenuIte
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        Fragment fragment = null;
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_collect) {
+            if (mCollectFragment == null){
+                mCollectFragment = new MyCollectFragment();
+                fragment = mCollectFragment;
+            }
+        } else if (id == R.id.nav_attention) {
+            if (mAttentionFragment == null){
+                mAttentionFragment = new MyAttentionFragment();
+                fragment = mAttentionFragment;
+            }
+        } else if (id == R.id.nav_posts) {
+            if (mPostsFragment == null){
+                mPostsFragment = new MyPostsFragment();
+                fragment = mPostsFragment;
+            }
+        } else if (id == R.id.nav_night_mode) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.nav_setting) {
+            if (mSettingFragment == null){
+                mSettingFragment = new SettingFragment();
+                fragment = mSettingFragment;
+            }
         }
+        transaction.replace(R.id.fl_content, fragment);
+        transaction.commit();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
---
+++ Metarial/app/src/main/java/com/jeongho/metarial/fragment/DiscoveryFragment.java
@@ -0,0 +1,23 @@
+package com.jeongho.metarial.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jeongho.metarial.R;
+
+/**
+ * Created by Jeongho on 16/6/16.
+ */
+public class DiscoveryFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.content_pager_3, container, false);
+        return v;
+    }
+}
