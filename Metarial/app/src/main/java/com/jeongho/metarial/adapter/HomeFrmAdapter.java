4be547cd28b21c52928033a60de993d3202508f8
JeonghoWin <zhfighting2014@163.com>
2016-06-18 15:48:49 +0800

    a

---
+++ Metarial/app/src/main/java/com/jeongho/metarial/adapter/HomeFrmAdapter.java
@@ -0,0 +1,47 @@
+package com.jeongho.metarial.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.jeongho.metarial.R;
+
+import java.util.List;
+
+/**
+ * Created by Jeongho on 2016/6/18.
+ */
+public class HomeFrmAdapter extends RecyclerView.Adapter<HomeFrmAdapter.ViewHolder>{
+
+    private List<String> mTitles;
+    public HomeFrmAdapter(List<String> list) {
+        mTitles = list;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+        ViewHolder holder = new ViewHolder(v);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mTextView.setText(mTitles.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTitles.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextView;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mTextView = (TextView) itemView.findViewById(R.id.item_tv);
+        }
+    }
+}
--- Metarial/app/src/main/java/com/jeongho/metarial/fragment/HomeFragment.java
+++ Metarial/app/src/main/java/com/jeongho/metarial/fragment/HomeFragment.java
@@ -3,21 +3,40 @@ package com.jeongho.metarial.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.jeongho.metarial.R;
+import com.jeongho.metarial.adapter.HomeFrmAdapter;
+
+import java.util.LinkedList;
 
 /**
  * Created by Jeongho on 16/6/16.
  */
 public class HomeFragment extends Fragment{
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.content_pager_1, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.home_rv);
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        LinkedList<String> list = new LinkedList<>();
+        for (int i = 0 ; i < 50; i++) {
+            list.add("galigeigei" + i);
+        }
+        mAdapter = new HomeFrmAdapter(list);
+        mRecyclerView.setAdapter(mAdapter);
+
         return v;
     }
 }
