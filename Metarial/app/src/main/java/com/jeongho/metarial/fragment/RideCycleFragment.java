eddc9200abd2db02a05bb870aba574c3eeeb4d25
JeonghoMac <zhfighting2014@163.com>
2016-07-25 22:39:09 +0800

    fix

--- Metarial/app/src/main/java/com/jeongho/metarial/fragment/RideCycleFragment.java
+++ Metarial/app/src/main/java/com/jeongho/metarial/fragment/RideCycleFragment.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,12 +16,20 @@ import android.widget.Button;
 import com.jeongho.metarial.R;
 import com.jeongho.metarial.activity.RideInfoActivity;
 import com.jeongho.metarial.activity.RideMeetDetailsActivity;
+import com.jeongho.metarial.adapter.HomeFrmAdapter;
+
+import java.util.LinkedList;
 
 /**
  * Created by Jeongho on 16/6/16.
  */
 public class RideCycleFragment extends Fragment implements View.OnClickListener {
 
+    private RecyclerView mRecyclerView;
+    private HomeFrmAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private GridLayoutManager mGridLayoutManager;
+    private SwipeRefreshLayout mRefreshLayout;
     private FloatingActionButton mRideFab;
 
     private Button mRideMeetBtn;
@@ -25,16 +37,36 @@ public class RideCycleFragment extends Fragment implements View.OnClickListener 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_ride, container, false);
+        View root = inflater.inflate(R.layout.fragment_ride, container, false);
 
-        mRideFab = (FloatingActionButton) v.findViewById(R.id.fab_ride);
+        mRideFab = (FloatingActionButton) root.findViewById(R.id.fab_ride);
         mRideFab.setOnClickListener(this);
 
-        mRideMeetBtn = (Button) v.findViewById(R.id.btn_ride_meet);
+
+
+        mRecyclerView = (RecyclerView) root.findViewById(R.id.rv_ride);
+        mLayoutManager = new LinearLayoutManager(getContext());
+//
+        mGridLayoutManager = new GridLayoutManager(getActivity(), 1);
+        mRecyclerView.setLayoutManager(mGridLayoutManager);
+//        //初始化cardView
+        LinkedList<String> list = new LinkedList<>();
+        for (int i = 0 ; i < 10; i++) {
+            list.add("galigeigei" + i);
+        }
+        mAdapter = new HomeFrmAdapter(getActivity(), list);
+        View header = inflater.inflate(R.layout.header_fragment_ride, null);
+        mAdapter.setHeaderView(header);
+        View footerView = LayoutInflater.from(getContext()).inflate(R.layout.item_footer, mRecyclerView, false);
+        mAdapter.setFooterView(footerView);
+
+        mRideMeetBtn = (Button) header.findViewById(R.id.btn_ride_meet);
         mRideMeetBtn.setOnClickListener(this);
-        mRoadBookBtn = (Button) v.findViewById(R.id.btn_road_book);
+        mRoadBookBtn = (Button) header.findViewById(R.id.btn_road_book);
         mRoadBookBtn.setOnClickListener(this);
-        return v;
+
+        mRecyclerView.setAdapter(mAdapter);
+        return root;
     }
 
     @Override
